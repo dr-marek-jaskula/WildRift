5aa0cf73005bce0879a19a00f6348ba621662251
jjcreator <jjcreator8@gmail.com>
2021-12-04 22:05:10 +0100

    champion types filtering

--- WWW/pages/champions/index.jsx
+++ WWW/pages/champions/index.jsx
@@ -2,35 +2,31 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED="0";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "../../styles/ChampionPage.module.scss";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Wrapper from "../../components/Wrapper";
 import { Checkbox, FormControlLabel, FormGroup } from "@mui/material";
 
 const ChampionsPage = (props) => {
 
-	const { darkmode, toggleDarkMode, championNames } = props;
+	const { darkmode, toggleDarkMode, championNamesAndTypes } = props;
 	const processToFilename = (string) => {
 		return string.replace(/[^a-zA-Z0-9]/g, "");
 	};
 	
-	const fixedChampionNames = championNames.map((item) => ({...item, params: {imgName: processToFilename(item.params.name), name: item.params.name}}));
+	const fixedChampionNames = championNamesAndTypes.map((champion) => ({...champion, imgName: processToFilename(champion.name)}));
 	const [ championList ] = useState(fixedChampionNames);
-	const [ championTypes ] = useState(["Assassin", "Defender", "Mage", "Marksman", "Support", "Warrior"]);
+	const [ championTypes ] = useState(["Assassin", "Fighter", "Mage", "Marksman", "Support", "Tank"]);
 	const [ filter, setFilter ] = useState("");
-	const initialChampionTypesObject = {};
-	
-	useEffect(() => {
-		championTypes.forEach(type => initialChampionTypesObject[type] = false);
-	}, []);
-
-	const [selectedTypes, setSelectedTypes] = useState(initialChampionTypesObject);
+	const [ selectedTypes, setSelectedTypes ] = useState([]);
 
 	const handleCheck = (e) => {
 		const item = e.target.value;
-		setSelectedTypes({
-			...selectedTypes,
-			[item]: !selectedTypes[item]
-		});
+		if (selectedTypes.some(type => type === item)) {
+			setSelectedTypes([...selectedTypes.filter(type => type != item)]);
+		}
+		else {
+			setSelectedTypes([...selectedTypes, item]);
+		}
 	};
 
 	const handleSearch = (e) => {
@@ -74,46 +70,57 @@ const ChampionsPage = (props) => {
 					</div>
 				</div>
 				<div className={styles.championList}>
-					{championList.filter(item => {
-						if (!filter.length) return true;
-						return item.params.name.toUpperCase().includes(filter.toUpperCase());
-					}).map((item, index) => {
-						const { params } = item;
-						const { name, imgName } = params;
-						return (
-							<Link href={`champions/${name}`} key={index}>
-								<div className={styles.championItem} >
-									<Image className={styles.championImage} width="120" height="120"  src={`/champions/thumbnails/${imgName}.png`}/>
-									<h3 className={styles.championName}>{name}</h3>
-								</div>
-							</Link>)
-						;}) }	
+					{championList
+						.filter(item => {
+							if (!filter.length) return true;
+							return item.name.toUpperCase().includes(filter.toUpperCase());
+						})
+						.filter(item => {
+							let condition = true;
+							if (selectedTypes.length == 0) {
+								return condition;
+							}
+							item.roles.forEach(role => {
+								if(selectedTypes.some(selectedRole => selectedRole == role)) {
+									condition = true;
+								}
+								else {
+									condition = false;
+								}
+							});
+							return condition;
+
+						})
+						.map((item, index) => {
+							const { name, imgName } = item;
+							return (
+								<Link href={`champions/${name}`} key={index}>
+									<div className={styles.championItem} >
+										<Image className={styles.championImage} width="120" height="120"  src={`/champions/thumbnails/${imgName}.png`}/>
+										<h3 className={styles.championName}>{name}</h3>
+									</div>
+								</Link>)
+							;}) }	
 				</div>
 			</main>
 		</Wrapper>
 	);
 };
 
-
 export default ChampionsPage;
 
 export async function getStaticProps() {
-	const req = await fetch("https://localhost:5001/api/Champion/Names", {
+	const req = await fetch("https://localhost:5001/api/Champion/NamesAndTypes", {
 		method: "GET",
 		mode: "cors",
 		cache: "default",
 		headers: {
 			"Content-Type":"application/json",
 		}});
-	const data = await req.json();
-	const championNames = data.map(name => {
-		return { params: {
-			name
-		}};
-	});
+	const championNamesAndTypes = await req.json();
 	return {
 		props: {
-			championNames
+			championNamesAndTypes
 		}
 	};
 }
